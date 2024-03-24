fa0772f83383c013a7c0e240cf1aabc3dfbdc090
Ryotaro Nakamura <ryotaro612.ooo@gmail.com>
2024-03-24 21:35:15 +0900

    styling

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import styles from './page.module.scss';
 
 export default function Home({}) {
-
-
-  return (
-    <>{process.env.NEXT_PUBLIC_DOGE}foobar
-    {process.argv}
-      Not signed in <br />
-      <button onClick={() => signIn('keycloak', {}, {kc_idp_hint: 'github'})}>Sign in</button>
-    </>
-  );
+  return <div className={styles.sso}>
+    <div>
+      <button className={styles.ssoButton}>Single-Sign On</button>
+    </div>
+    <div>
+      <span>Single-Sign On (kc_idp_hint)</span>
+      <input></input>
+    </div>
+    {/* <button onClick={() => signIn('keycloak', {}, { kc_idp_hint: 'github' })}>Sign in</button> */}
+  </div>
 }
